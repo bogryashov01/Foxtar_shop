d236138dcb163713a8b7ece8076cfe5fc788b835
YuliaGurkova <yulia1988sergeevna@gmail.com>
2019-06-10 23:26:51 +0300

    price range with styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-    $(".js-range-slider").ionRangeSlider();
+    $(".js-range-slider").ionRangeSlider({
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"        
+    }); 
 });
