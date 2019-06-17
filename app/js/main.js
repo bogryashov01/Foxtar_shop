335810783e69149f2f222c91043e228756cc02ae
maksymburiy <001masso@gmail.com>
2019-06-17 19:03:56 +0300

    fixed error

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,17 @@
 $(function () {
   
+  if($(".js-range-slider").length) {
     $(".js-range-slider").ionRangeSlider({
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$"        
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"        
     }); 
+  }
 
+
+  if($(".trending-products__slider").length) {
     $('.trending-products__slider').slick({
       infinite: true,
       slidesToShow: 4,
@@ -16,4 +20,5 @@ $(function () {
       dots: true,
       arrows: false,
     });
+  }
 });
