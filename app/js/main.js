0b3345600a64c3966c328f057ca003e5969d3f59
maksymburiy <001masso@gmail.com>
2019-06-29 14:33:32 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,32 @@
 $(function () {
-  
-  if($(".js-range-slider").length) {
+
+  if ($(".js-range-slider").length) {
     $(".js-range-slider").ionRangeSlider({
       min: 0,
       max: 1000,
       from: 0,
       to: 600,
-      prefix: "$"        
-    }); 
+      prefix: "$"
+    });
   }
 
 
-  if($(".trending-products__slider").length) {
+  if ($(".trending-products__slider").length) {
     $('.trending-products__slider').slick({
       infinite: true,
       slidesToShow: 4,
-      slidesToScroll: 4,
+      slidesToScroll: 1,
       adaptiveHeight: false,
       dots: true,
       arrows: false,
       responsive: [
         {
-          breakpoint:576,
+          breakpoint: 576,
           settings: {
-           slidesToShow: 1,
-           slidesToScroll: 1,
-         }
-        }]
+            slidesToShow: 1
+          }
+        }
+      ]
     });
   }
 });
